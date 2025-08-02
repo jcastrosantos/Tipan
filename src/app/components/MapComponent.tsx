88330bcb079f133d02e76866a9f66c49088e7c90
jcastrosantos <joaovitorcastrosantos44@gmail.com>
2025-08-01 22:54:32 -0300

    feat: add Marker on MapComponent

--- src/app/components/MapComponent.tsx
+++ src/app/components/MapComponent.tsx
@@ -1,17 +1,14 @@
-// src/components/MapComponent.tsx
-"use client"; // Esta diretiva é FUNDAMENTAL para que funcione no navegador
+"use client";
 
-import React, { useState, useCallback } from "react";
-import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
+import React, { useCallback } from "react";
+import { GoogleMap, useJsApiLoader, Marker } from "@react-google-maps/api";
 
 // Defina o tamanho padrão do mapa
 const containerStyle = {
   width: "90%",
-  height: "400px", // Ajuste a altura conforme sua necessidade
+  height: "400px",
 };
 
-// Defina a localização central inicial do mapa
-// Exemplo: Coordenadas da Tipan (você precisará ajustar para o endereço real)
 const center = {
   lat: -23.56805320096787, // Latitude de São Paulo (ajuste para o seu local)
   lng: -46.69369933020885, // Longitude de São Paulo (ajuste para o seu local)
@@ -25,21 +22,18 @@ const MapComponent: React.FC<MapComponentProps> = ({ apiKey }) => {
   const { isLoaded, loadError } = useJsApiLoader({
     id: "google-map-script",
     googleMapsApiKey: apiKey,
-    // libraries: ['places'] // Adicione bibliotecas adicionais se precisar (ex: 'places' para autocompletar)
   });
 
-  const [map, setMap] = useState<google.maps.Map | null>(null);
+  // const [map, setMap] = useState<google.maps.Map | null>(null);
 
   const onLoad = useCallback(function callback(map: google.maps.Map) {
-    // Você pode fazer algo com a instância do mapa aqui, se necessário
-    // Por exemplo, ajustar o zoom, adicionar marcadores, etc.
     const bounds = new window.google.maps.LatLngBounds(center);
-    map.fitBounds(bounds); // Ajusta o mapa para o centro inicial
-    setMap(map);
+    map.fitBounds(bounds);
+    // setMap(map);
   }, []);
 
   const onUnmount = useCallback(function callback(map: google.maps.Map) {
-    setMap(null);
+    setMap(map);
   }, []);
 
   if (loadError) return <div>Erro ao carregar o mapa: {loadError.message}</div>;
@@ -49,11 +43,10 @@ const MapComponent: React.FC<MapComponentProps> = ({ apiKey }) => {
     <GoogleMap
       mapContainerStyle={containerStyle}
       center={center}
-      zoom={15} // Nível de zoom inicial
+      zoom={15}
       onLoad={onLoad}
       onUnmount={onUnmount}
       options={{
-        // Opcional: desabilita controles padrão para um mapa mais limpo
         zoomControl: true,
         mapTypeControl: false,
         scaleControl: false,
@@ -61,7 +54,9 @@ const MapComponent: React.FC<MapComponentProps> = ({ apiKey }) => {
         rotateControl: false,
         fullscreenControl: false,
       }}
-    ></GoogleMap>
+    >
+      <Marker position={center} />
+    </GoogleMap>
   );
 };
 
